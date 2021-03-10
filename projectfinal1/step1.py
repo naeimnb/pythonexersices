fbee679c19920529f2afe9523703e6caa1603220
naeimnb <naeimn1987@gmail.com>
2021-03-10 11:24:49 +0330

    def hash and readCSV and CreatCS

--- projectfinal1/step1.py
+++ projectfinal1/step1.py
@@ -9,7 +9,25 @@ def hashin(myPassword):
 
 # print(hashin(input('')))
 
+
 # define rading csv file
+def read_csv(adres):
+    with open(adres) as f: 
+        lines=csv.reader(f) 
+        read_dic=dict()
+        for line in lines:
+            user_name=line[0]
+            for hash_pass in line[1:]:
+                read_dic[hash_pass]=user_name
+
+            print(read_dic)
 
 
+def creat_hash_csv(adres):
+    with open(adres,'w') as f: 
+        for i in range(1000,9999): 
+            print(hashin(str(i)),i ,file=f)
+
+read_csv('/home/naeim/Desktop/pythonexersices/projectfinal1/sample.csv')
+creat_hash_csv('/home/naeim/Desktop/pythonexersices/projectfinal1/hash_file.csv')
 
