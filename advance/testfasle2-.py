1e552bc55c29e1692efca61f93e4e67c48dc9feb
naeimnb <naeimn1987@gmail.com>
2021-03-11 17:15:57 +0330

    test prtintX2

--- advance/testfasle2-.py
+++ advance/testfasle2-.py
@@ -1,19 +1,13 @@
-class Computer:
-    count=0
-    def __init__(self, ram, hard, cpu):
-        self.ram=ram
-        self.hard=hard
-        self.cpu=cpu
-    def valu(self):
-        return(self.ram+self.hard+self.cpu)
+class maktabkhooneh:
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
 
-class Laptop(Computer):
-    def __init__(self, )
-
-
-pc1=Computer(12, 2, 4)
-pc2=Computer(12,1,6)
-print(pc1.valu())
-print(pc2.valu())
+    def value(self):
+        print (self.name + self.grade)
+        
+person = maktabkhooneh("Ahmad","Master",24)
+print(person.value())
 
 
