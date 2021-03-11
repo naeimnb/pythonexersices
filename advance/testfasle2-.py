b08b67c6dc02f1fb87baddc397327cad681ea887
naeimnb <naeimn1987@gmail.com>
2021-03-11 17:17:49 +0330

    test badi

--- advance/testfasle2-.py
+++ advance/testfasle2-.py
@@ -5,9 +5,7 @@ class maktabkhooneh:
         self.age = age
 
     def value(self):
-        return (self.name + self.grade)
+        return self.name + self.grade
         
 person = maktabkhooneh("Ahmad","Master",24)
 print(person.value())
-
-
