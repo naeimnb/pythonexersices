d3977c99c5e84056948e3a1c76c7414a763f5e05
naeimnb <naeimn1987@gmail.com>
2021-03-12 22:11:07 +0330

    install pycharm

--- old/4.py
+++ old/4.py
@@ -1,13 +1,13 @@
-max=0
-max2=0
+max = 0
+max2 = 0
 
-num=int(input(''))
-while num>0:
-    if max>num:
-        if max2<num:
-            max2=num
+num = int(input('hey'))
+while num > 0:
+    if max > num:
+        if max2 < num:
+            max2 = num
     else:
-        max2=max
-        max=num
-    num=int(input(''))
+        max2 = max
+        max = num
+    num = int(input(''))
 print(max, max2)
