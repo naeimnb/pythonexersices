c5d3d93d18aae7f017ddd3fa03dfbf0ed796a1ff
naeimnb <naeimn1987@gmail.com>
2021-03-14 00:21:44 +0330

    sakhte connection string va data entry to data base ba python va liblary mysql-connector

--- advance/testfasle3.py
+++ advance/testfasle3.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 con=mysql.connector.connect(user='niminimda', password='******', host='127.0.0.1', database='test')
 cursor=con.cursor()
-cursor.execute('INSERT INTO people VALUES (\'yasaman\',23,\'f\')')
+cursor.execute('INSERT INTO people VALUES (\'jasmine\',23,\'f\')')
 con.commit()
 cursor.close()
 con.close()
