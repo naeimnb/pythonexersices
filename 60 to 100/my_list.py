8c04642ff6357c08932ef9af20dfebd27c72857f
naeimnb <naeim.nobahari@gmail.com>
2021-04-21 06:34:02 -0400

    end part 2

--- 60 to 100/my_list.py
+++ 60 to 100/my_list.py
@@ -13,5 +13,43 @@ popis = names.pop()
 print(popis)
 names.insert(3, 'iran')
 del names[0]
+names.remove('mehrdad')
 for name in names:
     print(name)
+
+print('-------------------------------------------------------')
+
+languages = list()
+languages.append('farsi')
+languages.append('kurdish')
+languages.append('english')
+languages.append('thai')
+print(languages)
+print(languages.sort())
+languages.sort()
+print(languages)
+languages.sort(reverse=True)
+print(languages)
+print(sorted(languages))
+#print(languages)
+print(sorted(languages, reverse=True))
+
+print('-------------------------------------------------------')
+
+computer_languages = ['c', 'delphi', 'C', 'F', 'python', 'go']
+print(computer_languages)
+computer_languages.reverse()
+print(computer_languages)
+
+
+print('-------------------------------------------------------')
+
+alphabets = ['a','A', 'b', 'B','c','C', 'd', 'D','e','E', 'f', 'F']
+print(alphabets)
+alphabets.sort(reverse=True)
+print(alphabets)
+print(len(alphabets))
+print('b' < 'a')
+print('A' < 'a')
+print('B' < 'a')
+
