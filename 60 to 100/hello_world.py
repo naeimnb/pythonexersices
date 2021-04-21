47bb66aa0ffb0b991308d459bd103229eb8934ab
naeimnb <naeim.nobahari@gmail.com>
2021-04-21 06:03:38 -0400

    end part 1

--- 60 to 100/hello_world.py
+++ 60 to 100/hello_world.py
@@ -1,1 +1,29 @@
 print('hello world!')
+
+message = 'hello world again'
+print(message)
+
+message = 'hello world again and again'
+print(message)
+
+print(message.title())
+print(message.lower())
+print(message.upper())
+
+
+my_message1 = 'mesage one'
+my_message2 = 'message two'
+message = f"{my_message1} and {my_message2} are my mesages"
+print(message)
+
+print('this is the tab \t test')
+print('this is the new line \n test')
+
+message = '    my mesage with space   '
+print(message)
+print(message.rstrip())
+print(message.lstrip())
+print(message.strip())
+
+my_number = 1_000_000_000
+print(my_number)
