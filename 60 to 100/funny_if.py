6ac423c5db3103c49f6be7e1ff68c28da1e9cdd9
naeimnb <naeim.nobahari@gmail.com>
2021-04-22 14:37:22 -0400

    start & end part 4

---
+++ 60 to 100/funny_if.py
@@ -0,0 +1,19 @@
+my_items = [i for i in range(1,21)]
+print(my_items)
+
+if 5 in my_items:
+    print('yes i got 5')
+
+if 10 in my_items:
+    print('yes i got 10')
+
+if 15 in my_items:
+    print('yes i got 15')
+
+if 20 in my_items:
+    print('yes i got 20')
+
+if 25 in my_items:
+    print('yes i got 25')
+else:
+    print('no i did not find 25')
