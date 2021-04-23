eeca1989b6f4fee04667510de352167352c0911d
naeimnb <naeim.nobahari@gmail.com>
2021-04-23 07:03:43 -0400

    end testing json

--- 60 to 100/jason/jason.py
+++ 60 to 100/jason/jason.py
@@ -5,3 +5,8 @@ names = ['ali', 'mehrdad', 'naeim', 'masoud', 'omid']
 file_name = 'names.json'
 with open(file_name, 'w') as f:
     json.dump(names, f)
+
+
+with open (file_name) as f:
+    my_read = json.load(f)
+    print(my_read)
